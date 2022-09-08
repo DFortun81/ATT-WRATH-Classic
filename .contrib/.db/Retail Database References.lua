9925e0861b084b0620063d176d914f4f7af5b989
Crieve <dylanfortune@live.com>
2022-09-07 17:12:32 -0700

    Added Strand of the Ancients for Wrath Classic.

--- .contrib/.db/Retail Database References.lua
+++ .contrib/.db/Retail Database References.lua
@@ -150,6 +150,7 @@ root("PVP", n(-32, {	-- Honor Titles
 -- #IMPORT: 09 - PvP\\Battlegrounds\\02 Arathi Basin.lua
 -- #IMPORT: 09 - PvP\\Battlegrounds\\04 Eye of the Storm.lua
 -- #IMPORT: 09 - PvP\\Battlegrounds\\01 Warsong Gulch.lua
+-- #IMPORT: 09 - PvP\\Battlegrounds\\07 Strand of the Ancients.lua
 -- #IMPORT: 09 - PvP\\02 The Burning Crusade PvP Seasons.lua
 -- #IMPORT: 09 - PvP\\03 Wrath of the Lich King PvP Seasons.lua
 
