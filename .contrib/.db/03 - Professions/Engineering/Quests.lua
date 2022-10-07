41d39e654c1f21a2a0615924a6e15ea4190665de
Crieve <dylanfortune@live.com>
2022-10-07 14:13:19 -0700

    Updated Engineering quest format.

--- .contrib/.db/03 - Professions/Engineering/Quests.lua
+++ .contrib/.db/03 - Professions/Engineering/Quests.lua
@@ -10,8 +10,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 55.0, 8.6, STORMWIND_CITY },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = ALLIANCE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -33,8 +33,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 68.8, 45.2, IRONFORGE },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = ALLIANCE_ONLY,
 			["lvl"] = 30,
 			["cost"] = {
@@ -56,8 +56,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 62.6, 36.2, THE_BARRENS },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = ALLIANCE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -79,8 +79,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 75.6, 74.6, UNDERCITY },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = HORDE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -91,8 +91,8 @@ profession(ENGINEERING, {
 			["qg"] = 3494,	-- Tinkerwiz <Journeyman Engineer>
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 62.6, 36.2, THE_BARRENS },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = HORDE_ONLY,
 			["altQuests"] = {
 				3526,	-- Goblin Engineering
@@ -142,8 +142,9 @@ profession(ENGINEERING, {
 				{ "i", 4392, 2 },	-- Advanced Target Dummy
 				{ "i", 4407, 1 },	-- Accurate Scope
 			},
+			["timeline"] = { "added 5.0.1" },
 			["requireSkill"] = ENGINEERING,
-			["u"] = MOP_PHASE_ONE,
+			["repeatable"] = true,
 			["lvl"] = 20,
 		}),
 		q(29475, {	-- Goblin Engineering
@@ -178,8 +179,9 @@ profession(ENGINEERING, {
 				{ "i", 10507, 20 },	-- Solid Dynamite
 				{ "i", 4384, 5 },	-- Explosive Sheep
 			},
+			["timeline"] = { "added 5.0.1" },
 			["requireSkill"] = ENGINEERING,
-			["u"] = MOP_PHASE_ONE,
+			["repeatable"] = true,
 			["lvl"] = 20,
 		}),
 		q(3526, {	-- Goblin Engineering
@@ -197,8 +199,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 75.6, 74.6, UNDERCITY },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = HORDE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -220,8 +222,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 55.0, 8.6, STORMWIND_CITY },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = ALLIANCE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -243,8 +245,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 62.6, 36.2, THE_BARRENS },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = HORDE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -266,8 +268,8 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 68.8, 45.2, IRONFORGE },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.1.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = ALLIANCE_ONLY,
 			["cost"] = {
 				{ "i", 10789, 1 },	-- Manual of Engineering Disciplines
@@ -279,9 +281,9 @@ profession(ENGINEERING, {
 			["sourceQuest"] = 3640,	-- The Pledge of Secrecy
 			["altQuests"] = { 3639, 3643 },	-- Show Your Work
 			["description"] = "Requires 200 Engineering to start this quest.",
-			["requireSkill"] = ENGINEERING,
 			["coord"] = { 69.8, 50.2, IRONFORGE },
 			["timeline"] = { "removed 4.0.3.2000" },
+			["requireSkill"] = ENGINEERING,
 			["races"] = ALLIANCE_ONLY,
 			["cost"] = {
 				{ "i", 4392, 2 },	-- Advanced Target Dummy
@@ -299,9 +301,9 @@ profession(ENGINEERING, {
 			["altQuests"] = { 3639, 3641 },	-- The Pledge of Secrecy
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 28.2, 76.2, STRANGLETHORN_VALE },
+			["timeline"] = { "removed 4.0.3.2000" },
 			["requireSkill"] = ENGINEERING,
 			["races"] = HORDE_ONLY,
-			["timeline"] = { "removed 4.0.3.2000" },
 			["cost"] = {
 				{ "i", 4392, 2 },	-- Advanced Target Dummy
 				{ "i", 4407, 1 },	-- Accurate Scope
@@ -321,12 +323,33 @@ profession(ENGINEERING, {
 			},
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 54.4, 27.2, TANARIS },
-			["requireSkill"] = ENGINEERING,
 			["timeline"] = { "removed 4.0.3.2000" },
+			["requireSkill"] = ENGINEERING,
 			["cost"] = {
 				{ "i", 4392, 2 },	-- Advanced Target Dummy
 				{ "i", 4407, 1 },	-- Accurate Scope
 				{ "i", 10559, 6 },	-- Mithril Tube
+			},
+			["lvl"] = 30,
+		}),
+		q(3638, {	-- The Pledge of Secrecy
+			["qg"] = 8126,	-- Nixx Sprocketspring <Master Goblin Engineer>
+			["sourceQuests"] = {
+				3526,	-- Goblin Engineering
+				3629,	-- Goblin Engineering
+				3633,	-- Goblin Engineering
+				4181,	-- Goblin Engineering
+			},
+			["altQuests"] = {
+				3640,	-- The Pledge of Secrecy
+				3642,	-- The Pledge of Secrecy
+			},
+			["description"] = "Requires 200 Engineering to start this quest.",
+			["coord"] = { 54.4, 27.2, TANARIS },
+			["requireSkill"] = ENGINEERING,
+			["timeline"] = { "removed 4.0.3.2000" },
+			["cost"] = {
+				{ "i", 10792, 1 },	-- Nixx's Pledge of Secrecy
 			},
 			["lvl"] = 30,
 		}),
@@ -362,6 +385,7 @@ profession(ENGINEERING, {
 			["altQuests"] = { 3638, 3640 },	-- The Pledge of Secrecy
 			["description"] = "Requires 200 Engineering to start this quest.",
 			["coord"] = { 28.2, 76.2, STRANGLETHORN_VALE },
+			["timeline"] = { "removed 4.0.3.2000" },
 			["requireSkill"] = ENGINEERING,
 			["races"] = HORDE_ONLY,
 			["cost"] = {
@@ -373,27 +397,6 @@ profession(ENGINEERING, {
 					i(10794),	-- Oglethorpe's Signed Pledge
 				}),
 			},
-		}),
-		q(3638, {	-- The Pledge of Secrecy
-			["qg"] = 8126,	-- Nixx Sprocketspring <Master Goblin Engineer>
-			["sourceQuests"] = {
-				3526,	-- Goblin Engineering
-				3629,	-- Goblin Engineering
-				3633,	-- Goblin Engineering
-				4181,	-- Goblin Engineering
-			},
-			["altQuests"] = {
-				3640,	-- The Pledge of Secrecy
-				3642,	-- The Pledge of Secrecy
-			},
-			["description"] = "Requires 200 Engineering to start this quest.",
-			["coord"] = { 54.4, 27.2, TANARIS },
-			["requireSkill"] = ENGINEERING,
-			["timeline"] = { "removed 4.0.3.2000" },
-			["cost"] = {
-				{ "i", 10792, 1 },	-- Nixx's Pledge of Secrecy
-			},
-			["lvl"] = 30,
 		}),
 		applyclassicphase(TBC_PHASE_ONE, q(9636, {	-- The Zapthrottle Mote Extractor! [A]
 			["qg"] = 17634,	-- K. Lee Smallfry <Engineering Trainer>
@@ -423,5 +426,19 @@ profession(ENGINEERING, {
 				i(23888),	-- Schematic: Zapthrottle Mote Extractor
 			},
 		})),
+		-- Argus Questline
+		q(48069, {	-- The Wrench Calls
+			["qg"] = 125525,	-- Durael
+			["sourceQuest"] = 46941,	-- The Path Forward
+			["timeline"] = { "added 7.3.0.24614" },
+			["maps"] = {	-- actual possible coords would be preferred
+				831,	-- Upper Deck [The Vindicaar: Krokuun]
+				832,	-- Lower Deck [The Vindicaar: Krokuun]
+				883,	-- Upper Deck [The Vindicaar: Eredath]
+				884,	-- Lower Deck [The Vindicaar: Eredath]
+				886,	-- Upper Deck [The Vindicaar: Antoran Wastes]
+				887,	-- Lower Deck [The Vindicaar: Antoran Wastes]
+			},
+		}),
 	}),
 });
