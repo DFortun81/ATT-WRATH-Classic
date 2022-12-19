1b8eb4e836d1b96ac83f5e7c7d1159a99636e9f8
Crieve <dylanfortune@live.com>
2022-12-19 11:30:32 -0700

    A Winter Veil Gift (2009) is now Phase 4 Wrath Classic.

--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Darkshore.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Darkshore.lua
@@ -35,15 +35,15 @@ root("Zones", m(KALIMDOR, {
 				]]--
 			})),
 			-- #endif
+			prof(FISHING, {
+				i(12238),	-- Darkshore Grouper
+			}),
 			n(FLIGHT_PATHS, {
 				fp(26, {	-- Auberdine, Darkshore
 					["cr"] = 3841,	-- Caylais Moonfeather <Hippogryph Master>
 					["coord"] = { 36.4, 45.6, DARKSHORE },
 					["races"] = ALLIANCE_ONLY,
 				}),
-			}),
-			prof(FISHING, {
-				i(12238),	-- Darkshore Grouper
 			}),
 			n(QUESTS, {
 				q(986, {	-- A Lost Master (1/2)
