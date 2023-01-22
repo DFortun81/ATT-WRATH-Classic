3131a0824db3b7866f82ab7185ba6a6ef151d4a6
Crieve <dylanfortune@live.com>
2023-01-22 11:41:54 -0700

    Added the object debugger.

--- ATT-Classic.lua
+++ ATT-Classic.lua
@@ -14224,6 +14224,43 @@ app:GetWindow("ItemFinder", UIParent, function(self, ...)
 		if self.data.OnUpdate then self.data.OnUpdate(self.data); end
 	end
 end);
+app:GetWindow("Objects", UIParent, function(self, ...)
+	if self:IsVisible() then
+		if not self.initialized then
+			self.initialized = true;
+			local db = {};
+			db.text = "Object Debugger";
+			db.icon = app.asset("Achievement_Dungeon_GloryoftheRaider");
+			db.description = "This is a contribution debug tool. NOT intended to be used by the majority of the player base.";
+			db.visible = true;
+			db.expanded = true;
+			db.progress = 0;
+			db.total = 0;
+			db.back = 1;
+			self.data = db;
+			db.g = {};
+			local objectIDs = {};
+			for objectID,o in pairs(app.SearchForFieldContainer("objectID")) do
+				tinsert(objectIDs, tonumber(objectID));
+			end
+			insertionSort(objectIDs);
+			for _,objectID in ipairs(objectIDs) do
+				tinsert(db.g, app.CreateObject(objectID));
+			end
+			BuildGroups(db, db.g);
+		end
+		self.data.progress = 0;
+		self.data.total = 0;
+		
+		-- Update the groups without forcing Debug Mode.
+		local visibilityFilter = app.VisibilityFilter;
+		app.VisibilityFilter = app.NoFilter;
+		UpdateGroups(self.data, self.data.g);
+		UpdateWindow(self, ...);
+		if self.data.OnUpdate then self.data.OnUpdate(self.data); end
+		app.VisibilityFilter = visibilityFilter;
+	end
+end);
 app:GetWindow("RaidAssistant", UIParent, function(self)
 	if self:IsVisible() then
 		if not self.initialized then
