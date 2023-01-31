b9269ca36d7e6497f34637ab38cc91af65e76c18
Crieve <dylanfortune@live.com>
2023-01-30 17:07:57 -0700

    Duplicate map names no longer appear in the assocaited Maps list on the tooltip.

--- ATT-Classic.lua
+++ ATT-Classic.lua
@@ -11122,19 +11122,33 @@ local function RowOnEnter(self)
 				end
 				if not found then GameTooltip:AddLine(reference.description, 0.4, 0.8, 1, 1); end
 				if reference.maps then
-					local description = "Maps: ";
+					local description,maps,umaps,name = "Maps: ",{},{};
 					for i=1,#reference.maps,1 do
+						name = app.GetMapName(reference.maps[i]);
+						if name and not umaps[name] then
+							umaps[name] = true;
+							tinsert(maps, name);
+						end
+					end
+					for i,name in ipairs(maps) do
 						if i > 1 then description = description .. ", "; end
-						description = description .. app.GetMapName(reference.maps[i]);
+						description = description .. name;
 					end
 					GameTooltip:AddLine(" ", 1, 1, 1, 1);
 					GameTooltip:AddLine(description, 0.4, 0.8, 1, 1);
 				end
 			elseif reference.maps then
-				local description = "Maps: ";
+				local description,maps,umaps,name = "Maps: ",{},{};
 				for i=1,#reference.maps,1 do
+					name = app.GetMapName(reference.maps[i]);
+					if name and not umaps[name] then
+						umaps[name] = true;
+						tinsert(maps, name);
+					end
+				end
+				for i,name in ipairs(maps) do
 					if i > 1 then description = description .. ", "; end
-					description = description .. app.GetMapName(reference.maps[i]);
+					description = description .. name;
 				end
 				GameTooltip:AddLine(description, 0.4, 0.8, 1, 1);
 			end
