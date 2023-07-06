5d52c9147e4df54f8a14d7e277b7f55b89a79500
Crieve <dylanfortune@live.com>
2023-07-06 11:17:03 -0700

    Fixed and moved /attquests to its own UI Module file.

---
+++ src/UI/Quests.lua
@@ -0,0 +1,33 @@
+-- App locals
+local appName, app = ...;
+
+-- Implementation
+app:GetWindow("Quests", {
+	parent = UIParent,
+	Silent = true,
+	OnInit = function(self)
+		SLASH_ATTQUESTS1 = "/attquests";
+		SlashCmdList["ATTQUESTS"] = function(cmd)
+			self:Toggle();
+		end
+	end,
+	OnRebuild = function(self, ...)
+		if not self.data then
+			self.data = {
+				text = "Quests",
+				icon = "Interface\\GossipFrame\\AvailableQuestIcon",
+				description = "This window shows you all of the quests (based on filters) that you can complete. Go get 'em!\n\nNOTE: This window will not include quest items used to complete quests, but will show all of the associated quest rewards.",
+				visible = true, 
+				expanded = true,
+				back = 1,
+				OnUpdate = function(data)
+					local g = app:BuildSearchResponseForField(app:GetDataCache().g, "questID");
+					if #g > 0 then
+						data.g = g;
+						data.OnUpdate = nil;
+					end
+				end
+			};
+		end
+	end,
+});
